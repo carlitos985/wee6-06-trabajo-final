6aee8ad4faf22828cc2f47ea73c29336034ebf93
Carlitos H <carbono985@gmail.com>
2024-04-24 11:34:05 -0500

    Correcciones 1

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,18 +1,18 @@
 
 const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
 const express = require('express');
-const verifyJwt=require('../utils/verifyJWT')
+const {verifyJWT}= require('../utils/verifyJWT')
 
 
 const routerUser = express.Router();
 
 routerUser.route('/')
-    .get(verifyJwt,getAll)
-    .post(verifyJwt,create);
+    .get(verifyJWT,getAll)
+    .post(create);
 
 routerUser.route('/:id')
-    .delete(remove)
-    .put(update);
+    .delete(verifyJWT,remove)
+    .put(verifyJWT,update);
 
 routerUser.route('/login')
     .post(login)
